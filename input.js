7f3cf921c28cd4c901cdf7f8a880cdaad84b1cf5
kenkla <k@zva.us>
2015-10-29 13:16:35 -0400

    added index.html and script

---
+++ input.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	
+	$("#phone-home").keydown(function (e) {
+		// Allow backspace, delete, tab, escape, enter
+		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+			//Allow Ctrl+A
+			(e.keyCode == 65 && e.ctrlKey === true) ||
+			//Allow Ctrl+C
+			(e.keyCode == 67 && e.ctrlKey === true) ||
+			//Allow Ctrl+X
+			(e.keyCode == 88 && e.ctrlKey === true) ||
+			//Allow home, end, left, right
+			(e.keyCode >= 35 && e.keyCode <= 39)) {
+				return;
+			}
+			
+		// Stop keypress if it is not a number
+		if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+			e.preventDefault();
+		}
+		
+	});
+	
+});
