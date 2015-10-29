b8d2043db5a1491008dc8848be0adde3d32be629
kenkla <k@zva.us>
2015-10-29 16:01:47 -0400

    added behaviors to input.js

--- input.js
+++ input.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 	
-	$("#phone-home").keydown(function (e) {
+	var phoneNumHome = $("#phone-home"),
+		phoneNumCell = $("#phone-cell"),
+		phoneNum = $(".phone-entry"),
+		zipCode = $(".zipcode-entry"),
+		numberOnly = $(".number-only");
+	
+	phoneNum.attr("maxlength", 10);
+	zipCode.attr("maxlength", 9);
+	
+	numberOnly.keydown(function (e) {
 		// Allow backspace, delete, tab, escape, enter
 		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
-			//Allow Ctrl+A
+			//Allow Ctrl+A, Ctrl+C, Ctrl+X
 			(e.keyCode == 65 && e.ctrlKey === true) ||
-			//Allow Ctrl+C
 			(e.keyCode == 67 && e.ctrlKey === true) ||
-			//Allow Ctrl+X
 			(e.keyCode == 88 && e.ctrlKey === true) ||
 			//Allow home, end, left, right
 			(e.keyCode >= 35 && e.keyCode <= 39)) {
@@ -19,6 +26,27 @@ $(document).ready(function() {
 			e.preventDefault();
 		}
 		
+		// Insert hyphens into home phone number
+		var tempStr = phoneNumHome.val();
+		if (tempStr.length == 10 && (parseInt(tempStr) != NaN)) {
+			var standardized = tempStr.substring(0, 3) + "-" + tempStr.substring(3, 6) + "-" + tempStr.substring(6, 10);
+			phoneNumHome.val(standardized);
+		};
+		
+		// Insert hyphens into cell phone number
+		var tempStr = phoneNumCell.val();
+		if (tempStr.length == 10 && (parseInt(tempStr) != NaN)) {
+			var standardized = tempStr.substring(0, 3) + "-" + tempStr.substring(3, 6) + "-" + tempStr.substring(6, 10);
+			phoneNumCell.val(standardized);
+		};
+		
+		//Insert hyphens into ZIP Code
+		var tempStr = zipCode.val();
+		if (tempStr.length == 9 && !(tempStr.indexOf("-") > 0)) {
+			var standardized = tempStr.substring(0, 5) + "-" + tempStr.substring(5, 9);
+			zipCode.val(standardized);
+		};	
+				
 	});
 	
 });
